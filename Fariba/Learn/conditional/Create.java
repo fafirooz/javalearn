6dcaa822110104cebbf9ef39b3c7b75d357058bb
fafirooz <fa.firooz@gmail.com>
2023-05-04 15:45:58 -0700

    add model

---
+++ Fariba/Learn/conditional/Create.java
@@ -0,0 +1,16 @@
+package Fariba.Learn.conditional;
+
+public class Create {
+    int a;
+    char b;
+
+
+    public static void main(String[] args) {
+
+        Create create1 = new Create();
+        System.out.println(create1.a);
+        System.out.println(create1.b);
+    }
+
+}
+
