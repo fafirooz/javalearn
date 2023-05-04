7149afc64e2760712a95744652af9ca3c949aa8f
fafirooz <fa.firooz@gmail.com>
2023-05-04 16:15:12 -0700

    add model

---
+++ Fariba/Learn/thinking/season1/ATypeName.java
@@ -0,0 +1,10 @@
+package Fariba.Learn.thinking.season1;
+
+public class ATypeName {
+
+    public static void main(String[]args){
+
+        ATypeName atypename = new ATypeName();
+        System.out.println("ATypeName");
+    }
+}
