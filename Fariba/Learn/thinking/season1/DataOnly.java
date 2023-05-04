b3f78c33ffc4636cf9b447a2e8446882da3ccbef
fafirooz <fa.firooz@gmail.com>
2023-05-04 16:07:21 -0700

    add model

---
+++ Fariba/Learn/thinking/season1/DataOnly.java
@@ -0,0 +1,17 @@
+package Fariba.Learn.thinking.season1;
+
+public class DataOnly {
+    int i;
+    double d;
+    boolean b;
+
+    public static void main(String[] args){
+
+        DataOnly dataonly = new DataOnly();
+
+        System.out.println(dataonly.i);
+        System.out.println(dataonly.d);
+        System.out.println(dataonly.b);
+
+    }
+}
