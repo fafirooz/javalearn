64f8cbcdc5fbb32c9c6cb4925c4b2e5802bea66e
fafirooz <fa.firooz@gmail.com>
2023-05-09 19:17:13 -0700

    add model

---
+++ Fariba/Learn/net/Days.java
@@ -0,0 +1,23 @@
+package Fariba.Learn.net;
+
+public class Days {
+
+    public static void main(String[] args) {
+        String[] days = new String[7];
+        days[0] = "sunday";
+        days[1] = "monday";
+        days[2] = "tuesday";
+        days[3] = "wednesday";
+        days[4] = "thursday";
+        days[5] = "friday";
+        days[6] = "saturday";
+
+
+        System.out.println(days.length);
+        System.out.println(days[days.length - 1]);
+        System.out.println(days[days.length - 3]);
+
+    }
+}
+
+
