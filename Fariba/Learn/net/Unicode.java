fbdde9a21ad194d02054ec47b663cf39c4beb081
fafirooz <fa.firooz@gmail.com>
2023-05-09 22:04:12 -0700

    add model

---
+++ Fariba/Learn/net/Unicode.java
@@ -0,0 +1,11 @@
+package Fariba.Learn.net;
+
+public class Unicode {
+    public static void main(String[] args) {
+        char mi = 'J';
+        char bi= 'P';
+        char fi= 'R';
+        System.out.println(mi+ bi + fi);
+        System.out.println("mi+ bi + fi");
+    }
+}
