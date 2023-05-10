3cdc211a1761d28edca8837e38301325895a1caa
fafirooz <fa.firooz@gmail.com>
2023-05-10 09:58:49 -0700

    add model

---
+++ Fariba/Learn/net/Operators.java
@@ -0,0 +1,22 @@
+package Fariba.Learn.net;
+
+public class Operators {
+    public static void main(String[] args) {
+        int speed = 30;
+        int speedLimited = 45;
+
+        System.out.println("speed is " + speed);
+        System.out.println("speedLimited is " + speedLimited);
+
+        boolean result = speed > speedLimited;
+        System.out.println("speed > speedLimited is ");
+        System.out.println(result);
+
+        boolean result1 = speed < speedLimited;
+        System.out.println("speed > speedLimited is ");
+        System.out.println(result1);
+
+
+    }
+}
+
